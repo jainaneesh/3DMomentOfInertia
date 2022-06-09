d65ecc5dffd2108d29355290310fb0d15babdff1
jainaneesh <jainaneesh15@gmail.com>
2022-06-09 18:03:13 -0400

    Added cuboid/cube

--- 3DObjectsMOI.py
+++ 3DObjectsMOI.py
@@ -44,17 +44,55 @@ def cylinder(mass,length,radius,offx,offy,offz):
     return I
 
 
+def cylinder(mass,length,offx,offy,offz):
+    I = np.zeros((3,3),dtype=np.float64)
+    density = mass/(length[0]*length[1]*length[2])
 
-mass = float(input("Enter the mass: "))
-length = float(input("Enter the length: "))
-radius1 = float(input("Enter the inner radius: "))
-radius2 = float(input("Enter the outer radius: "))
-radius = [radius1,radius2]
+    # Ixx
+    fint = lambda x,y,z: 1*(y**2+z**2)
+    resxx, errxx = integrate.tplquad(fint,-length[2]/2,length[2]/2,lambda z:-length[1]/2,length[1]/2,lambda y,z:-length[0]/2,lambda y,z:length[0]/2)
+    Ixx = (resxx*density)+(pow(offy,2)+pow(offz,2))*mass
+    I[0][0]=Ixx
+
+    # Iyy
+    fint = lambda x,y,z: 1*(x**2+z**2)
+    resyy, erryy = integrate.tplquad(fint,-length[2]/2,length[2]/2,lambda z:-length[1]/2,length[1]/2,lambda y,z:-length[0]/2,lambda y,z:length[0]/2)
+    Iyy = (resyy*density)+(pow(offx,2)+pow(offz,2))*mass
+    I[1][1]=Iyy
+
+    # Izz
+    fint = lambda x,y,z: 1*(x**2+y**2)
+    reszz, errzz = integrate.tplquad(fint,-length[2]/2,length[2]/2,lambda z:-length[1]/2,length[1]/2,lambda y,z:-length[0]/2,lambda y,z:length[0]/2)
+    Izz = (reszz*density)+(pow(offx,2)+pow(offy,2))*mass
+    I[2][2]=Izz
+
+    print(I)
+    return I
+
+
+
+# mass = float(input("Enter the mass of the cylinder: "))
+# length = float(input("Enter the length of the cylinder: "))
+# radius1 = float(input("Enter the inner radius of the cylinder: "))
+# radius2 = float(input("Enter the outer radius of the cylinder: "))
+# radius = [radius1,radius2]
+# offsetx = float(input("Enter x offset distance: "))
+# offsety = float(input("Enter y offset distance: "))
+# offsetz = float(input("Enter z offset distance: "))
+
+# I = cylinder(mass,length,radius,offsetx,offsety,offsetz)
+
+mass = float(input("Enter the mass of the cube: "))
+length1 = float(input("Enter the length 1(Length 1 is along the x axis): "))
+length2 = float(input("Enter the length 2(Length 2 is along the y axis): "))
+length3 = float(input("Enter the length 3(Length 3 is along the z axis): "))
+
+length = [length1,length2,length3]
 offsetx = float(input("Enter x offset distance: "))
 offsety = float(input("Enter y offset distance: "))
 offsetz = float(input("Enter z offset distance: "))
 
-I = cylinder(mass,length,radius,offsetx,offsety,offsetz)
+I = cylinder(mass,length,offsetx,offsety,offsetz)
 
 
 
