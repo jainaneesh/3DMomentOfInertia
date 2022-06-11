48fbd237d3011edc2342af7994fb7d1b0b7374d3
jainaneesh <jainaneesh15@gmail.com>
2022-06-11 13:15:36 -0400

    Added more content

--- 3DObjectsMOI.py
+++ 3DObjectsMOI.py
@@ -5,69 +5,86 @@ import matplotlib.pyplot as plt
 import math
 import numpy as np
 
-def cylinder(mass,length,radius,offx,offy,offz):
-    I = np.zeros((3,3),dtype=np.float64)
-    density = mass/(math.pi*(pow(radius[1],2)-pow(radius[0],2))*length)
+class cylinder:
+    def __init__(self,mass,length,radius,offx,offy,offz):
+        self.mass = mass
+        self.length = length
+        self.radius = radius
+        self.offx = offx
+        self.offy = offy
+        self.offz = offz
 
-    # Ixx
-    theta = smp.symbols('theta')
-    z = smp.symbols('z')
-    r = smp.symbols('r')
-    fsym = z**2*r+r**3*(smp.sin(theta))**2
-    fint = smp.lambdify([z,r,(smp.sin(theta))],fsym)
-    fint = lambda z,r,theta:z**2*r+r**3*(smp.sin(theta))**2
-    resxx,errxx = integrate.tplquad(fint,0,2*math.pi,lambda theta:radius[0],lambda theta:radius[1],lambda r,theta:-length/2,lambda r,theta:length/2)
-    Ixx = (resxx*density)+(pow(offy,2)+pow(offz,2))*mass
-    I[0][0] = Ixx
+    def cylinderMOI(self,mass,length,radius,offx,offy,offz):
+        I = np.zeros((3,3),dtype=np.float64)
+        self.density = mass/(math.pi*(pow(radius[1],2)-pow(radius[0],2))*length)
 
-    # Iyy
-    theta = smp.symbols('theta')
-    z = smp.symbols('z')
-    r = smp.symbols('r')
-    fsym = z**2*r+r**3*(smp.cos(theta))**2
-    fint = smp.lambdify([z,r,(smp.cos(theta))],fsym)
-    fint = lambda z,r,theta:z**2*r+r**3*(smp.cos(theta))**2
-    resyy,erryy = integrate.tplquad(fint,0,2*math.pi,lambda theta:radius[0],lambda theta:radius[1],lambda r,theta:-length/2,lambda r,theta:length/2)
-    Iyy = (resyy*density)+(pow(offz,2)+pow(offx,2))*mass
-    I[1][1] = Iyy
+        # Ixx
+        theta = smp.symbols('theta')
+        z = smp.symbols('z')
+        r = smp.symbols('r')
+        fsym = z**2*r+r**3*(smp.sin(theta))**2
+        fint = smp.lambdify([z,r,(smp.sin(theta))],fsym)
+        fint = lambda z,r,theta:z**2*r+r**3*(smp.sin(theta))**2
+        resxx,errxx = integrate.tplquad(fint,0,2*math.pi,lambda theta:radius[0],lambda theta:radius[1],lambda r,theta:-length/2,lambda r,theta:length/2)
+        Ixx = (resxx*self.density)+(pow(offy,2)+pow(offz,2))*mass
+        I[0][0] = Ixx
 
-    # Izz
-    prod = 2*math.pi*length*density
-    f = lambda r:r**3
-    rlower = 0
-    rupper = radius
-    reszz, errzz = integrate.quad(f,radius[0],radius[1])
-    Izz = (prod*reszz)+(pow(offx,2)+pow(offy,2))*mass
-    I[2][2] = Izz  
+        # Iyy
+        theta = smp.symbols('theta')
+        z = smp.symbols('z')
+        r = smp.symbols('r')
+        fsym = z**2*r+r**3*(smp.cos(theta))**2
+        fint = smp.lambdify([z,r,(smp.cos(theta))],fsym)
+        fint = lambda z,r,theta:z**2*r+r**3*(smp.cos(theta))**2
+        resyy,erryy = integrate.tplquad(fint,0,2*math.pi,lambda theta:radius[0],lambda theta:radius[1],lambda r,theta:-length/2,lambda r,theta:length/2)
+        Iyy = (resyy*self.density)+(pow(offz,2)+pow(offx,2))*mass
+        I[1][1] = Iyy
 
-    print(I)
-    return I
+        # Izz
+        prod = 2*math.pi*length*self.density
+        f = lambda r:r**3
+        rlower = 0
+        rupper = radius
+        reszz, errzz = integrate.quad(f,radius[0],radius[1])
+        Izz = (prod*reszz)+(pow(offx,2)+pow(offy,2))*mass
+        I[2][2] = Izz  
+
+        print(I)
+        return I
 
 
-def cube(mass,length,offx,offy,offz):
-    I = np.zeros((3,3),dtype=np.float64)
-    density = mass/(length[0]*length[1]*length[2])
 
-    # Ixx
-    fint = lambda x,y,z: 1*(y**2+z**2)
-    resxx, errxx = integrate.tplquad(fint,-length[2]/2,length[2]/2,lambda z:-length[1]/2,length[1]/2,lambda y,z:-length[0]/2,lambda y,z:length[0]/2)
-    Ixx = (resxx*density)+(pow(offy,2)+pow(offz,2))*mass
-    I[0][0]=Ixx
+class cube:
+    def __init__(self,mass,length,offx,offy,offz):
+        self.mass = mass
+        self.length = length
+        self.offx = offx
+        self.offy = offy
+        self.offz = offz
+    def cubeMOI(self,mass,length,offx,offy,offz):
+        I = np.zeros((3,3),dtype=np.float64)
+        self.density = mass/(length[0]*length[1]*length[2])
 
-    # Iyy
-    fint = lambda x,y,z: 1*(x**2+z**2)
-    resyy, erryy = integrate.tplquad(fint,-length[2]/2,length[2]/2,lambda z:-length[1]/2,length[1]/2,lambda y,z:-length[0]/2,lambda y,z:length[0]/2)
-    Iyy = (resyy*density)+(pow(offx,2)+pow(offz,2))*mass
-    I[1][1]=Iyy
+        # Ixx
+        fint = lambda x,y,z: 1*(y**2+z**2)
+        resxx, errxx = integrate.tplquad(fint,-length[2]/2,length[2]/2,lambda z:-length[1]/2,length[1]/2,lambda y,z:-length[0]/2,lambda y,z:length[0]/2)
+        Ixx = (resxx*self.density)+(pow(offy,2)+pow(offz,2))*mass
+        I[0][0]=Ixx
 
-    # Izz
-    fint = lambda x,y,z: 1*(x**2+y**2)
-    reszz, errzz = integrate.tplquad(fint,-length[2]/2,length[2]/2,lambda z:-length[1]/2,length[1]/2,lambda y,z:-length[0]/2,lambda y,z:length[0]/2)
-    Izz = (reszz*density)+(pow(offx,2)+pow(offy,2))*mass
-    I[2][2]=Izz
+        # Iyy
+        fint = lambda x,y,z: 1*(x**2+z**2)
+        resyy, erryy = integrate.tplquad(fint,-length[2]/2,length[2]/2,lambda z:-length[1]/2,length[1]/2,lambda y,z:-length[0]/2,lambda y,z:length[0]/2)
+        Iyy = (resyy*self.density)+(pow(offx,2)+pow(offz,2))*mass
+        I[1][1]=Iyy
 
-    print(I)
-    return I
+        # Izz
+        fint = lambda x,y,z: 1*(x**2+y**2)
+        reszz, errzz = integrate.tplquad(fint,-length[2]/2,length[2]/2,lambda z:-length[1]/2,length[1]/2,lambda y,z:-length[0]/2,lambda y,z:length[0]/2)
+        Izz = (reszz*self.density)+(pow(offx,2)+pow(offy,2))*mass
+        I[2][2]=Izz
+
+        print(I)
+        return I
 
 
 print("Select the operation")
@@ -89,8 +106,8 @@ while (operation==1):
         offsetx = float(input("Enter x offset distance: "))
         offsety = float(input("Enter y offset distance: "))
         offsetz = float(input("Enter z offset distance: "))
-
-        I = cylinder(mass,length,radius,offsetx,offsety,offsetz)
+        cyl = cylinder(mass,length,radius,offsetx,offsety,offsetz)
+        I = cyl.cylinderMOI(mass,length,radius,offsetx,offsety,offsetz)
         
     elif (object==2):
         mass = float(input("Enter the mass of the cube: "))
@@ -101,8 +118,8 @@ while (operation==1):
         offsetx = float(input("Enter x offset distance: "))
         offsety = float(input("Enter y offset distance: "))
         offsetz = float(input("Enter z offset distance: "))
-
-        I = cube(mass,length,offsetx,offsety,offsetz)
+        cube = cube(mass,length,offsetx,offsety,offsetz)
+        I = cube.cubeMOI(mass,length,offsetx,offsety,offsetz)
 
     print("Another object?")
     print("1) Yes")
@@ -118,7 +135,8 @@ while (operation==2):
     print("Which body do you want first: ")
     print("1) Cylinder")
     print("2) Cube/cuboid")
-    comp = int(input("Enter the first body: "))
+    comp = int(input("Enter the first body: ")) 
+    
 
 
 
